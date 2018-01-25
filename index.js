cde0a6ccf412e811c2f227972e7c010197275d41
Robert Lego <robertlego@gmail.com>
2018-01-25 03:10:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase());
 }
+
+function logWhisper(string) {
+  console.log(string.toUpperCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string == lowercase) {
+    retrun "I can/'t hear you!"
+  } else if (string == uppercase) {
+    return "YES INDEED!"
+  } else if (string = "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
