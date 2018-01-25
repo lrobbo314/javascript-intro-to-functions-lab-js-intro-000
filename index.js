7fe6e124c3aa033cb3d4e1242790dbfafd2445f2
Robert Lego <robertlego@gmail.com>
2018-01-25 03:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() == 'hello') {
-    return "YES INDEED!"
-  } else if (string.toUpperCase() == 'HELLO') {
     return "I can\'t hear you!"
+  } else if (string.toUpperCase() == 'HELLO') {
+    return "YES INDEED!"
   } else if (string = "I love you, Grandma.") {
     return "I love you, too."
   }
